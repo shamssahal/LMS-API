0ef7ce2a37b6911729f909b3d308382991a9b58a
Sahal Shams <shamssahal@gmail.com>
2022-08-29 02:10:41 +0530

    Fix: CORS issue

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
 
     const { origin } = req.headers;
     if (allowedOrigins.indexOf(origin) > -1) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
+        res.setHeader('Access-Control-Allow-Origin', '*');
     }
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
